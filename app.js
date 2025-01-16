06fafc259e8a748dfeefce063499ca114589b11e
gozde35colakoglu <gozde35colakoglu@gmail.com>
2025-01-16 13:14:21 +0300

    db connect

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv";
+import conn from "./db.js";
 
-const app = express()
-const port = 3000
+dotenv.config();
+
+//connection to the db
+conn();
+
+const app = express();
+const port = process.env.PORT;
 
 //ejs template engine
 app.set("view engine", 'ejs');
@@ -9,13 +16,13 @@ app.set("view engine", 'ejs');
 //static files middleware
 app.use(express.static('public'));
 
-app.get("/",  (req,res) => {
+app.get("/", (req, res) => {
     res.render("index");
 });
-app.get("/about",  (req,res) => {
+app.get("/about", (req, res) => {
     res.render("about");
 });
 
 app.listen(port, () => {
-    console.log("application running on port: ${port}");
-    });
+    console.log('application running on port: ${port}');
+});
